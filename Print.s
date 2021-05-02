c6bce02f894c729133d712f7265d25dca18908db
Raffer Li <li.raffer0@gmail.com>
2021-05-02 10:12:07 -0500

    first commit

---
+++ Print.s
@@ -0,0 +1,53 @@
+; Print.s
+; Student names: change this to your names or look very silly
+; Last modification date: change this to the last modification date or look very silly
+; Runs on TM4C123
+; EE319K lab 7 device driver for any LCD
+;
+; As part of Lab 7, students need to implement these LCD_OutDec and LCD_OutFix
+; This driver assumes two low-level LCD functions
+; SSD1306_OutChar   outputs a single 8-bit ASCII character
+; SSD1306_OutString outputs a null-terminated string 
+
+    IMPORT   SSD1306_OutChar
+    IMPORT   SSD1306_OutString
+    EXPORT   LCD_OutDec
+    EXPORT   LCD_OutFix
+    PRESERVE8
+    AREA    |.text|, CODE, READONLY, ALIGN=2
+    THUMB
+
+
+
+;-----------------------LCD_OutDec-----------------------
+; Output a 32-bit number in unsigned decimal format
+; Input: R0 (call by value) 32-bit unsigned number
+; Output: none
+; Invariables: This function must not permanently modify registers R4 to R11
+LCD_OutDec
+
+
+      BX  LR
+;* * * * * * * * End of LCD_OutDec * * * * * * * *
+
+; -----------------------LCD _OutFix----------------------
+; Output characters to LCD display in fixed-point format
+; unsigned decimal, resolution 0.01, range 0.00 to 9.99
+; Inputs:  R0 is an unsigned 32-bit number
+; Outputs: none
+; E.g., R0=0,    then output "0.00 "
+;       R0=3,    then output "0.03 "
+;       R0=89,   then output "0.89 "
+;       R0=123,  then output "1.23 "
+;       R0=999,  then output "9.99 "
+;       R0>999,  then output "*.** "
+; Invariables: This function must not permanently modify registers R4 to R11
+LCD_OutFix
+
+     BX   LR
+ 
+     ALIGN
+;* * * * * * * * End of LCD_OutFix * * * * * * * *
+
+     ALIGN          ; make sure the end of this section is aligned
+     END            ; end of file
